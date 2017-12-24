fe1ea0ce95460e08b8d6b0b64620c8e6fc5b5967
Cjesse <cjesse1992@mail.com>
2017-12-24 10:43:56 -0500

    u

---
+++ client/src/app/mock-problems.ts
@@ -0,0 +1,16 @@
+import { Problem } from "./models/problem.model";
+
+export const PROBLEMS: Problem[] = [
+{
+  id: 1,
+  name: "haha",
+  desc: "hahahs",
+  difficulty: "easy"
+},
+{
+  id: 2,
+  name: "hehe",
+  desc: "hhehehs",
+  difficulty: "medium"
+}];
+
